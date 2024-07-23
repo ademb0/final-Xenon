a05b98a440d9eb3961fa1745ad7d0195d0553b31
ademb0 <adembenothmen8@gmail.com>
2024-07-23 17:02:07 +0100

    first commit

---
+++ mains.js
@@ -0,0 +1,564 @@
+// data base
+
+let headphonesTypes = [
+  {
+    image:
+      "https://m.media-amazon.com/images/I/71dIzi7vTLL._AC_SX679_.jpg",
+    namE: "Pauroty 60% Wireless Mechanical",
+    price: 79.99,
+    description: "Keyboard, Triple Mode 2.4G/USB-C/Bluetooth Gaming Keyboard, RGB Backlit",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://images.kabum.com.br/produtos/fotos/105009/teclado-mecanico-gamer-hyperx-alloy-origins-core-rgb-hx-kb7rdx-br_1574693479_g.jpg",
+    namE: "teclado-mecanico-hyperx",
+    price: 59.99,
+    description: "Wireless keyboard",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/818wdhDpbTL._AC_SX425_.jpg",
+    namE: "SteelSeries Apex Pro",
+    price: 199.99,
+    description: "Mini Wireless clavier gaming HyperMagnetic - Format compact 60%",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://th.bing.com/th/id/OIP.V9Vyb-jjlIs6haxBW2LqwAAAAA?rs=1&pid=ImgDetMain",
+    namE: "corsair-gaming-k57",
+    price: 60.99,
+    description: "rgb-bluetooth-keyboard-with-rgb-back-lighting-black",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/51HCGV0I0vL.jpg",
+    namE: "migliori-tastiera-gaming",
+    price: 249.99,
+    description: "migliori-tastiera-gaming wireless keyboard",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://th.bing.com/th/id/OIP.mEL7aKOYeqlLtjcPOpZMMQHaE8?rs=1&pid=ImgDetMain",
+    namE: "logitech k673",
+    price: 229.99,
+    description:
+      "the best wireless keyboard",
+    color: "red",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81W8mZ7RZ9L._AC_SX425_.jpg",
+    namE: "Corsair K70",
+    price: 69.99,
+    description: "lavier Mécanique Gaming sans Fil (Cherry MX Red, Rétro-Éclairage LED Bleu, AZERTY) Noir ",
+    color: "white",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/61KqgHvkHWS._AC_SX425_.jpg",
+    namE: "Razer BlackWidow V3 ",
+    price: 149.99,
+    description: "clavier Gamer Mécanique Sans-Fil Compact à 65% ",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81dQzKkeEIL._AC_SX425_.jpg",
+    namE: "Razer DeathStalker V2 Pro",
+    price: 199.99,
+    description: " Clavier Gamer Optique Sans-Fil à Profil Bas",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://th.bing.com/th/id/R.9055c1054c9f9feada9a51e002bdf4e4?rik=ZHaEiVlTDMYhSg&pid=ImgRaw&r=0",
+    namE: "Logitech G915",
+    price: 99.99,
+    description: "lIGHTSPEED Clavier Gamer sans Fil Mécanique,",
+    color: "white",
+    quantity: 1,
+  },
+];
+let EarphonesTypes = [
+  {
+    image:
+      "https://global-img.gamergen.com/logitech-g513-mechanical-gaming-keyboard-silver-keys_0900888355.jpg",
+    namE: "Logitech G LIGHTSYNC",
+    price: 199.99,
+    description: "un clavier mécanique et des enceintes avec des couleurs dévoilés",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://10-raisons.fr/wp-content/uploads/2020/08/10-meilleurs-claviers-mecaniques-pour-taper-plus-rapidement.jpg",
+    namE: "Redragon K552 ",
+    price: 49.9,
+    description: "Redragon fournit des claviers pour taper plus rapidement",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image:
+      "https://10-raisons.fr/wp-content/uploads/2020/08/1598032411_811_10-meilleurs-claviers-mecaniques-pour-taper-plus-rapidement.jpg",
+    namE: "Logitech Orion",
+    price: 199.99,
+    description: "Ce clavier est conçu pour avoir une activation 25% plus rapide.",
+    color: "blue",
+    quantity: 1,
+  },
+  {
+    image: "https://10-raisons.fr/wp-content/uploads/2020/08/1598032411_195_10-meilleurs-claviers-mecaniques-pour-taper-plus-rapidement.jpg",
+    namE: "Razer Cynosa",
+    price: 39.99,
+    description: "le Razer Ornata est fantastique pour le confort",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://10-raisons.fr/wp-content/uploads/2020/08/1598032411_906_10-meilleurs-claviers-mecaniques-pour-taper-plus-rapidement.jpg",
+    namE: "Redragon K556",
+    price: 79.99,
+    description: "Un autre bon candidat pour un clavier mécanique dans un format compact est le K556",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://10-raisons.fr/wp-content/uploads/2020/08/1598032411_536_10-meilleurs-claviers-mecaniques-pour-taper-plus-rapidement.jpg",
+    namE: "Clavier mécanique Pictek",
+    price: 99.99,
+    description: "Il est également compatible avec Windows et Mac.",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://10-raisons.fr/wp-content/uploads/2020/08/1598032411_452_10-meilleurs-claviers-mecaniques-pour-taper-plus-rapidement.jpg",
+    namE: "SteelSeries Apex Hybrid",
+    price: 49.99,
+    description: "Il utilise un alliage d'aluminium de qualité aéronautique pour le cadre",
+    color: "balck",
+    quantity: 1,
+  },
+  {
+    image: "https://10-raisons.fr/wp-content/uploads/2020/08/1598032411_954_10-meilleurs-claviers-mecaniques-pour-taper-plus-rapidement.jpg",
+    namE: "Razer BlackWidow",
+    price: 89.99,
+    description: "Il est également conçu pour la vitesse grâce aux fonctions de bosse tactile",
+    color: "balck",
+    quantity: 1,
+  },
+  {
+    image: "redragonk630gamingbudgetkeyboard_1024x1024@2x.webp",
+    namE: "Redragon-Dragonborn-K630 ",
+    price: 89.99,
+    description: "gaming-keyboard ",
+    color: "balck",
+    quantity: 1,
+  },
+];
+
+let speakersTypes = [
+  {
+    image: "https://m.media-amazon.com/images/I/615BonaBHBL.__AC_SX300_SY300_QL70_ML2_.jpg",
+    namE: "THE G-LAB Combo ARGON E  ",
+    price: 49.99,
+    description: "Clavier Gamer AZERTY Rétroéclairé, Souris Gamer 3200 DPI, Micro Casque Gaming, Tapis de Souris Antidérapant",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/5142nPpab6L._AC_SX425_.jpg",
+    namE: "Pack de rétroéclairage LED Filaire",
+    price: 39.99,
+    description:
+      "Ensemble de Clavier et de Souris de Jeu",
+    color: "white",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71IPm9ffdnL._AC_SX425_.jpg",
+    namE: "G-Lab Combo Yttrium ",
+    price: 29.99,
+    description:
+      "Pack Clavier et Souris Gaming, Combo 3 en 1 avec Clavier Gaming QWERTY, 105 Touches, Souris Gamer 2600 DPI, Tapis de Souris Gamer",
+    color: "red",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/61XeSiRMdQL.__AC_SX300_SY300_QL70_ML2_.jpg",
+    namE: "Konix Drakkar Pack",
+    price: 39.99,
+    description: "Clavier filaire AZERTY rétroéclairage LED - Souris filaire 102 g - Tapis de souris viking",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/61GII4RUJNL._AC_SX425_.jpg",
+    namE: "pack logitech",
+    price: 39.99,
+    description:
+      "Souris Logitec G502 X,Clavier LIGHTSYNC RVB G815",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/81ra670xPzL._AC_SX425_.jpg",
+    namE: "RedThunder K62",
+    price: 69.99,
+    description: "Clavier et Souris de Jeu sans Fil RVB",
+    color: "black",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/61qNDRxuLKL._AC_SX679_.jpg",
+    namE: "Trust Gaming GXT",
+    price: 49.99,
+    description: " Clavier Azerty français, Souris, Tapis et Casque avec Micro",
+    color: "brown",
+    quantity: 1,
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/512neF3vsaL._AC_SX425_.jpg",
+    namE: "Cooler Master MS110",
+    price: 99.99,
+    description: "RGB Clavier & Souris Gaming Combo. ",
+    color: "balck",
+    quantity: 1,
+  },
+];
+
+function createCard(product) {
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.classList.add("swiper-slide");
+  card.innerHTML = `
+  `;
+  return card;
+}
+
+function showProducts(products, cardsContainer, addToCardFunction) {
+  products.forEach(function (value, key) {
+    let productCard = createCard(value);
+    productCard.innerHTML = `<figure class='image-product'>
+            <img src="${value.image}">
+            <i class="fa-solid fa-heart"onclick="changeHeartColor(this)"></i>
+          </figure>
+          <div class="name-price">
+            <h4>${value.namE}</h4>
+            <h4>$${value.price}</h4>
+          </div>
+          <p>${value.description}</p>
+          <button onclick ="${addToCardFunction}(${key})" class="dodo">Add to cart</button>
+        </div>
+    `;
+    cardsContainer.appendChild(productCard);
+  });
+}
+
+let cards1 = document.querySelector(".cards1");
+let cards2 = document.querySelector(".cards2");
+let cards3 = document.querySelector(".cards3");
+
+showProducts(headphonesTypes, cards1, "addToCard");
+showProducts(EarphonesTypes, cards2, "addToEarphonesCard");
+showProducts(speakersTypes, cards3, "addToSpeakersCard");
+
+var swiper1 = new Swiper(".slider1", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  grabCursor: "true",
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    720: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+    1350: {
+      slidesPerView: 4,
+    },
+  },
+});
+var swiper2 = new Swiper(".slider2", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  grabCursor: "true",
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    720: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+    1350: {
+      slidesPerView: 4,
+    },
+  },
+});
+var swiper3 = new Swiper(".slider3", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  grabCursor: "true",
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    720: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+    1350: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+function changeHeartColor(PinkHeart) {
+  PinkHeart.classList.toggle("pinkheart");
+}
+
+// add to card
+
+let cardBuying = document.querySelector(".cart-buying");
+
+let cardBuyingg = JSON.parse(localStorage.getItem("data")) || [];
+
+console.log(cardBuyingg);
+
+function addToCard(key) {
+  if (cardBuyingg[key] == null) {
+    cardBuyingg[key] = headphonesTypes[key];
+    cardBuyingg[key].totalPrice = cardBuyingg[key].price;
+  }
+  reloadCard();
+}
+function addToEarphonesCard(key) {
+  if (cardBuyingg[key] == null) {
+    cardBuyingg[key] = EarphonesTypes[key];
+    cardBuyingg[key].totalPrice = cardBuyingg[key].price;
+  }
+  reloadCard();
+}
+
+// Add Speakers to Cart
+function addToSpeakersCard(key) {
+  if (cardBuyingg[key] == null) {
+    cardBuyingg[key] = speakersTypes[key];
+    cardBuyingg[key].totalPrice = cardBuyingg[key].price;
+  }
+  reloadCard();
+}
+
+function addQuantity(key) {
+  cardBuyingg[key].quantity++;
+  cardBuyingg[key].totalPrice =
+    cardBuyingg[key].quantity * cardBuyingg[key].price;
+  reloadCard();
+}
+
+function minesQuantity(key) {
+  if (cardBuyingg[key].quantity > 1) {
+    cardBuyingg[key].quantity--;
+    cardBuyingg[key].totalPrice =
+      cardBuyingg[key].quantity * cardBuyingg[key].price;
+  } else {
+    delete cardBuyingg[key];
+  }
+  reloadCard();
+}
+
+function reloadCard() {
+  let listCards = document.querySelector(".listcards");
+  listCards.innerHTML = "";
+  let totalPrice = 0;
+
+  cardBuyingg.forEach(function (value, key) {
+    if (value != null) {
+      let slectedItem = document.createElement("div");
+      slectedItem.classList.add("add-card");
+      slectedItem.innerHTML += `
+        <figure class="add-img">
+            <img src="${value.image}">
+        </figure>
+        <div class="name-price space">
+            <h4>${value.namE}</h4>
+            <h4>$${value.price}</h4>
+        </div>
+        <div class="plus-mines">
+            <div class="plus"><i class="fa-solid fa-plus" onclick ="addQuantity(${key})"></i></div>
+            <div class="mines" ><i class="fa-solid fa-minus" onclick ="minesQuantity(${key})"></i></div>
+            <div class="mini-totale"><p>$${value.totalPrice}</p></div>
+        </div>
+        `;
+      listCards.appendChild(slectedItem);
+      totalPrice += value.totalPrice;
+    }
+  });
+
+  let totalePrice = document.querySelector(".totalePrice");
+  totalePrice.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
+  localStorage.setItem("data", JSON.stringify(cardBuyingg));
+}
+
+function displayCartItems() {
+  let listCards = document.querySelector(".listcards");
+  listCards.innerHTML = "";
+  let totalPrice = 0;
+
+  cardBuyingg.forEach(function (value, key) {
+    if (value != null) {
+      let slectedItem = document.createElement("div");
+      slectedItem.classList.add("add-card");
+      slectedItem.innerHTML += `
+        <figure class="add-img">
+            <img src="${value.image}">
+        </figure>
+        <div class="name-price space">
+            <h4>${value.namE}</h4>
+            <h4>$${value.price}</h4>
+        </div>
+        <div class="plus-mines">
+            <div class="plus"><i class="fa-solid fa-plus" onclick="addQuantity(${key})"></i></div>
+            <div class="mines"><i class="fa-solid fa-minus" onclick="minesQuantity(${key})"></i></div>
+            <div class="mini-totale"><p>$${value.totalPrice}</p></div>
+        </div>
+        `;
+      listCards.appendChild(slectedItem);
+      totalPrice += value.totalPrice;
+    }
+  });
+
+  let totalePrice = document.querySelector(".totalePrice");
+  totalePrice.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
+}
+
+// Call the function to display the cart items when the page loads
+displayCartItems();
+/// showing the product in the add card
+
+let closeCard = document.getElementsByClassName("fa-x");
+let openCard = document.getElementsByClassName("fa-cart-plus");
+let card = document.querySelector(".cards");
+let searchBar = document.querySelector(".search-bar");
+let searchIcon = document.querySelector(".fa-magnifying-glass");
+
+closeCard[0].onclick = function () {
+  cardBuying.style.right = "-100%";
+};
+
+openCard[0].onclick = function () {
+  cardBuying.style.right = "0%";
+};
+
+function openBarsearch() {
+  searchBar.classList.toggle("active");
+  accountLogIn.classList.remove("active");
+  accountSignup.classList.remove("active");
+  responsiveNavBar.classList.remove("active");
+}
+function openAccountLog() {
+  accountLogIn.classList.toggle("active");
+  searchBar.classList.remove("active");
+  accountSignup.classList.remove("active");
+  responsiveNavBar.classList.remove("active");
+}
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+let accountLogIn = document.querySelector(".loginNow");
+let accountSignup = document.querySelector(".sign-up");
+let signUp = document.querySelector("#signup"); // Fix the id selector
+let login = document.querySelector("#login");
+
+login.addEventListener("click", (e) => {
+  e.preventDefault();
+  accountSignup.classList.remove("active");
+  accountLogIn.classList.toggle("active");
+});
+
+signUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  accountSignup.classList.toggle("active");
+  accountLogIn.classList.remove("active");
+  responsiveNavBar.classList.remove("active");
+});
+
+let barIcon = document.querySelector("#menu");
+let responsiveNavBar = document.querySelector(".responsive-navBar");
+
+barIcon.addEventListener("click", () => {
+  responsiveNavBar.classList.toggle("active");
+  searchBar.classList.remove("active");
+  accountLogIn.classList.remove("active");
+  accountSignup.classList.remove("active");
+});
+
+
+const sliderContainer = document.querySelector(".sliderContainer");
+const leftButton = document.querySelector(".left-button");
+const rightButton = document.querySelector(".right-button");
+let scrollPosition = 0;
+
+leftButton.addEventListener("click", () => {
+  scrollPosition -= 430; // Adjust the value based on your content's width
+  sliderContainer.scrollTo({ left: scrollPosition, behavior: "smooth" });
+});
+
+rightButton.addEventListener("click", () => {
+  scrollPosition += 430; // Adjust the value based on your content's width
+  sliderContainer.scrollTo({ left: scrollPosition, behavior: "smooth" });
+});
